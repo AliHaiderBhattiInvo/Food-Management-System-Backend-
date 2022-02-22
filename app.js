09b2d78aa527ca2189150ac5d4fa0c7b825e37eb
Ali Haider Bhatti <ali.haider@invozone.com>
2022-02-23 02:01:51 +0500

    image uploader implemented

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer')
 const bodyParser = require('body-parser');
 const authRoutes = require("./server/routes/auth")
 const cors=require('cors');
@@ -13,6 +14,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.use("/api", authRoutes)
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './server/images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+app.use(express.static(__dirname + '/public'));
+app.use('/server/images', express.static('server/images'));
+
+
+app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+    // req.file is the `profile-file` file
+    // req.body will hold the text fields, if there were any
+    console.log(JSON.stringify(req.file))
+    var response = `localhost:5000/`
+    // response += "Files uploaded successfully.<br>"
+    response += `${req.file.path}`
+    return res.send(response)
+  })
 
 const port = process.env.PORT || 5000
 app.listen(port,()=>{
